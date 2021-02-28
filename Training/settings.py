28f651b0bd569a34f3876cbac84728b0b8d220a8
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2021-02-28 07:17:56 +0000

    Fault isolation. Getting Bad Gateway (400) from Heroku.

--- Training/settings.py
+++ Training/settings.py
@@ -16,9 +16,11 @@ import environ
 import braintree
 import dj_database_url
 
+"""
 root = environ.Path(__file__) - 3  # get root of the project
 env = environ.Env()
 environ.Env.read_env()  # reading .env file
+"""
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
