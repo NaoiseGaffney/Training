6db99364bf5e20db68d2ed0223907667a11a2782
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2021-02-28 06:18:16 +0000

    Fixed type in 'settings.py'.

--- Training/settings.py
+++ Training/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'Training.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in.os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
