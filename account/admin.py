5364ef32bfb945ff5e1f4bcd5a6a69de76cb8ec6
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2021-03-05 01:01:49 +0000

    Documentation: added images and PDF's refered to in 'READEME.md'.
    Testing new models for rating and comments. Will move these to a new Django App instead.

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Comment
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'address', 'post_code', 'city']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'orders', 'rating']
+    search_fields = ('rating', 'comment')
--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from orders.models import Order
 
 
 class Profile(models.Model):
@@ -11,3 +12,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
+class Comment(models.Model):
+    CHOICES = [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=CHOICES)
+    comment = models.CharField(max_length=250, blank=True, null=True)
