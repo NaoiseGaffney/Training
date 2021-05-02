bacc9dd8e1ba4f2c64a5a8e64c7162bc32144ff7
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2021-05-02 01:17:13 +0100

    Removed '# SECURITY WARNING: don't run with debug turned on in production! # DEBUG = os.environ.get('DEBUG')' from 'settings.py'. Hardcoded 'Debug = False' to ensure there are no issues in production.

--- Training/settings.py
+++ Training/settings.py
@@ -34,9 +34,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG')
-
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
