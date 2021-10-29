ff86d46c67a59c468fd8fe436530dbc4e22dd070
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2021-10-29 18:40:23 +0100

    Delete .venv/lib/python3.8/site-packages/braintree directory

--- .venv/lib/python3.8/site-packages/braintree/exceptions/request_timeout_error.py
+++
@@ -1,7 +0,0 @@
-from braintree.exceptions.braintree_error import BraintreeError
-
-class RequestTimeoutError(BraintreeError):
-    """
-    Raised when a client request timeout occurs.
-    """
-    pass
--- .venv/lib/python3.8/site-packages/braintree/exceptions/service_unavailable_error.py
+++
@@ -1,7 +0,0 @@
-from braintree.exceptions.braintree_error import BraintreeError
-
-class ServiceUnavailableError(BraintreeError):
-    """
-    Raised when the gateway is unavailable.
-    """
-    pass
